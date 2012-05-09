248cc5e38c5e064cfefb5e55e25d576299384957
Kevin McMahon <kmcmahon@redpointtech.com>
2012-05-09 07:25:29 -0500

    Initial Commit

---
+++ LocationDemoTests/LocationDemoTests.h
@@ -0,0 +1,13 @@
+//
+//  LocationDemoTests.h
+//  LocationDemoTests
+//
+//  Created by Kevin McMahon on 5/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface LocationDemoTests : SenTestCase
+
+@end
---
+++ LocationDemoTests/LocationDemoTests.m
@@ -0,0 +1,32 @@
+//
+//  LocationDemoTests.m
+//  LocationDemoTests
+//
+//  Created by Kevin McMahon on 5/9/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "LocationDemoTests.h"
+
+@implementation LocationDemoTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in LocationDemoTests");
+}
+
+@end
